65d34aa1813b86d74a29024525ce399e2806c68b
mhbahmani <mhbahmani79@gmail.com>
2019-04-16 11:13:17 +0430

    shopMenuCommandsChecker func added

--- src/com/company/Views/ConsoleInput.java
+++ src/com/company/Views/ConsoleInput.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 public class ConsoleInput {
     private static Scanner scanner = new Scanner(System.in);
+
     enum Menu {MAIN, ACCOUNT, COLLECTION, SHOP, BATTLE}
+
     Menu menu;
+    private static String command;
+
     public static void mainMenuCommandsChecker() {
 
     }
@@ -19,7 +23,24 @@ public class ConsoleInput {
     }
 
     public static void shopMenuCommandsChecker() {
+        command = scanner.nextLine();
+        if(command.matches("show")){
+            //show
+        }else if(command.matches("help")){
+            //help
+        }else if(command.matches("exit")){
+            //exit
+        }else if(command.matches("search \\d+")){
+            //search
+        }else if(command.matches("show collection")){
+            //show collection
+        }else if(command.matches("search collection")){
+            //search collection
+        }else if(command.matches("buy \\d+")){
 
+        }else if(command.matches("sell \\d+")){
+
+        }
     }
 
     public static void battleMenuCommandsChecker() {
