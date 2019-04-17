655b2154a4865bc5d212b5ac0fefceb0ef79799e
mhbahmani <mhbahmani79@gmail.com>
2019-04-17 13:51:45 +0430

    cardExistsInShop and getCardIDByName funcs added to ShopController

--- src/com/company/Controllers/ShopController.java
+++ src/com/company/Controllers/ShopController.java
@@ -1,5 +1,26 @@
 package com.company.Controllers;
 
+import com.company.Models.Card.Card;
+import com.company.Models.Shop;
+
+
 public class ShopController {
 
+    public static boolean cardExistsInShop(String cardName) {
+        for (Card card : Shop.getShopCollection().getCards()) {
+            if (card.getName().equals(cardName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int getCardIdByName(String cardName) {
+        for (Card card : Shop.getShopCollection().getCards()) {
+            if (card.getName().equals(cardName)) {
+                return card.getId();
+            }
+        }
+        return -1;//that means there is no card with this name
+    }
 }
