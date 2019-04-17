e82c246de078c3feb3643dfa9aadc341d6828747
mhbahmani <mhbahmani79@gmail.com>
2019-04-18 02:01:58 +0430

    NOTENOUGH_DRAKE added to ErrorType

--- src/com/company/Models/ErrorType.java
+++ src/com/company/Models/ErrorType.java
@@ -4,6 +4,7 @@ public enum ErrorType {
     USERNAME_EXISTS("An account already exists with this username."),
     USERNAME_NOTFOUND("This username does not exists"),
     CARD_NOTFOUND("This card does not exists"),
+    NOTENOUGH_DRAKE("you have not enough drake to buy this card"),
     PASSWORD_INVALID("Invalid password.");
 
     private String message;
