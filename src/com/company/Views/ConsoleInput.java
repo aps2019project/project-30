02bd97ce6d475ba0a31d9a43e8926ad2b8a1e39a
mhbahmani <mhbahmani79@gmail.com>
2019-04-18 15:01:42 +0430

    show commnad handled in ShopMneuCommandsChecker in ConsoleInput

--- src/com/company/Views/ConsoleInput.java
+++ src/com/company/Views/ConsoleInput.java
@@ -84,7 +84,7 @@ public class ConsoleInput {
 
     public static void shopMenuCommandsChecker(String command) {
         if (command.matches("show")) {
-            //show
+            ShopView.showAll();
         } else if (command.matches("exit")) {
             //exit
         } else if (command.matches("search [a-zA-Z]+")) {
