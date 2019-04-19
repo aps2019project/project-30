816c6556ded1d0f20c0af1e43554cc86b14d5db9
HaMiD <hamidrezash2000@gmail.com>
2019-04-19 19:38:44 +0430

    BattleController Added To Controllers

--- src/com/company/Controllers/BattleController.java
+++ src/com/company/Controllers/BattleController.java
@@ -1,4 +1,11 @@
 package com.company.Controllers;
 
+import com.company.Models.Battle.Battle;
+
 public class BattleController {
+    Battle battle;
+
+    public BattleController(Battle battle) {
+        this.battle = battle;
+    }
 }
--- src/com/company/Models/Battle/Battle.java
+++ src/com/company/Models/Battle/Battle.java
@@ -1,11 +1,34 @@
 package com.company.Models.Battle;
 
+import com.company.Controllers.BattleController;
 import com.company.Models.Battle.Modes.Mode;
 import com.company.Models.User.Player;
 
 public abstract class Battle {
     private Mode mode;
     private Player turnToPlay;
+    private BattleController battleController;
+
+    public Battle(Mode mode) {
+        this.mode = mode;
+        this.battleController = new BattleController(this);
+    }
+
+    public Mode getMode() {
+        return mode;
+    }
+
+    public Player getTurnToPlay() {
+        return turnToPlay;
+    }
+
+    public BattleController getBattleController() {
+        return battleController;
+    }
+
+    public void setTurnToPlay(Player turnToPlay) {
+        this.turnToPlay = turnToPlay;
+    }
 
     public abstract void handler();
 }
