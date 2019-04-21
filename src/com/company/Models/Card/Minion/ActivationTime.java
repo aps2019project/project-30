46691e068277e6101bbcdbe9b3d0badbea6f57cd
HaMiD <hamidrezash2000@gmail.com>
2019-04-21 20:18:25 +0430

    Item Class Added In Model

--- src/com/company/Models/Card/Minion/ActivationTime.java
+++ src/com/company/Models/Card/Minion/ActivationTime.java
@@ -1,4 +1,4 @@
 package com.company.Models.Card.Minion;
 
-public enum  ActivationTime {PASSIVE, ON_ATTACK, ON_RESPAWN, ON_DEATH, ON_DEFEND}
+public enum  ActivationTime {PASSIVE, ON_ATTACK, ON_RESPAWN, ON_DEATH, ON_DEFEND, COMBO}
 
