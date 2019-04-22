54ee414f7807c863dada0bc6a12e5593404a7256
mhbahmani <mhbahmani79@gmail.com>
2019-04-22 11:54:01 +0430

    CollectionController linked to Collection

--- src/com/company/Models/Card/Groups/Collection.java
+++ src/com/company/Models/Card/Groups/Collection.java
@@ -1,14 +1,20 @@
 package com.company.Models.Card.Groups;
 
+import com.company.Controllers.CollectionController;
 import com.company.Models.Card.Card;
 
 import java.util.ArrayList;
 
 public class Collection {
     private ArrayList<Card> cards;
-    public  ArrayList<Card> getCards() {
-        return cards;
+    private CollectionController collectionController;
+
+    public CollectionController getCollectionController() {
+        return collectionController;
     }
 
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
 
 }
