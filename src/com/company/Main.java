909fc64bbb907988f057247dbb35448187868cc4
HaMiD <hamidrezash2000@gmail.com>
2019-04-23 22:20:11 +0430

    sample spell with jsonConverter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,26 @@
 package com.company;
 
+import com.company.Models.Buff.Buff;
+import com.company.Models.Buff.HolyBuff;
+import com.company.Models.Card.Minion.Minion;
+import com.company.Models.Card.Spell.Spell;
+import com.company.Models.TargetType;
 import com.company.Views.ConsoleInput;
+import com.google.gson.Gson;
 
 public class Main {
 
     public static void main(String[] args) {
-        ConsoleInput.menusHandler();
+//        ConsoleInput.menusHandler();
+        Buff holyBuff = new HolyBuff(null,1,0,1);
+        Spell spell = new Spell();
+        spell.setName("Total Disarm");
+        spell.setPriceInDrake(1000);
+        spell.setManaPoint(0);
+        spell.getBuffsToCast().add(holyBuff);
+        spell.setInGraveCards(false);
+        spell.setTargetType(TargetType.ATTACKTYPE_MELEE);
+        Gson gson = new Gson();
+        gson.toJson(spell);
     }
 }
--- src/com/company/Models/Card/Spell/Spell.java
+++ src/com/company/Models/Card/Spell/Spell.java
@@ -5,15 +5,6 @@ import com.company.Models.Card.Card;
 import com.company.Models.Card.Hero.Hero;
 
 public class Spell extends Card {
-    private SpellType spellType;
-
-    public Spell(SpellType spellType) {
-        this.spellType = spellType;
-    }
-
-    public SpellType getSpellType() {
-        return spellType;
-    }
 
     public void throwBuffs(Card target) {
         target.getBuffsCasted().addAll(this.getBuffsToCast());
