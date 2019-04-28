b675f77a03b4a20cdfd83f9333bc03bfe4e117c0
mhbahmani <mhbahmani79@gmail.com>
2019-04-29 02:21:43 +0430

    printGameInfo method added to BattleView (mode info need to add to this method)

--- src/com/company/Views/BattleView.java
+++ src/com/company/Views/BattleView.java
@@ -1,11 +1,18 @@
 package com.company.Views;
 
+import com.company.Models.Battle.Battle;
 import com.company.Models.Card.Card;
 import com.company.Models.Card.Minion.Minion;
 
 import java.util.ArrayList;
 
 public class BattleView {
+    Battle battle;
+
+    public BattleView(Battle battle) {
+        this.battle = battle;
+    }
+
     public static void printMinionsInfo(ArrayList<Minion> minions) {
         minions.forEach(minion ->
                 System.out.printf(
@@ -15,9 +22,19 @@ public class BattleView {
                         minion.getCell().getxCoordinate(),
                         minion.getCell().getyCoordinate(),
                         minion.getAttackPower()
-                        )
+                )
         );
     }
+
+    public void printGameInfo() {
+        for (int playerIndex = 0; playerIndex < 2; playerIndex++) {
+            System.out.printf(
+                    "Player %d: \n" +
+                            "manas: %d\n", battle.getPlayers()[playerIndex].getMana());
+        }
+        //mode info
+    }
+
 
     public static void printGraveYardCards(ArrayList<Card> cards) {
         //Todo : printGraveYardCards : View
