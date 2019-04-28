a5d0a54ce3608b598ee36205cc09c76c5ea2b4f9
mhbahmani <mhbahmani79@gmail.com>
2019-04-28 11:14:19 +0430

    clone and createNewId added to Minion and Card

--- src/com/company/Models/Card/Minion/Minion.java
+++ src/com/company/Models/Card/Minion/Minion.java
@@ -89,4 +89,19 @@ public class Minion extends Card implements Soldier {
         }
         return false;
     }
+
+    public Minion clone(){
+        Minion minion = new Minion();
+        minion.setDescription(this.getDescription());
+        minion.setInGraveCards(this.isInGraveCards());
+        minion.setName(this.getName());
+        minion.setManaPoint(this.getManaPoint());
+        minion.setPriceInDrake(this.getPriceInDrake());
+        minion.setHealth(this.getHealth());
+        minion.setxCoordiante(this.getxCoordiante());
+        minion.setyCoordinate(this.getyCoordinate());
+        minion.setTargetType(this.getTargetType());
+        minion.setId(Card.createNewId());
+        return minion;
+    }
 }
