434d88b75e2d1f9aeb42f6a6e8712635d6e36149
mhbahmani <mhbahmani79@gmail.com>
2019-04-30 15:37:51 +0430

    HealthWeaknessBuff class created and completely implemented

---
+++ src/com/company/Models/Buff/HealthWeaknessBuff.java
@@ -0,0 +1,33 @@
+package com.company.Models.Buff;
+
+import com.company.Models.Card.Hero.Hero;
+import com.company.Models.Card.Minion.Minion;
+
+public class HealthWeaknessBuff extends Buff{
+
+    public HealthWeaknessBuff(Type antiBuff, int remTurnToBeInactive, int remTurnToCast, int value) {
+        super(antiBuff, remTurnToBeInactive, remTurnToCast, value);
+        super.name = Name.HEALTH_WEAKNESS;
+        super.type = Type.POSSETIVE;
+    }
+
+    @Override
+    public void cast(){
+        if(isActive()){
+            if(canCastThisTurn()){
+                if (super.cardToCast instanceof Hero)
+                    ((Hero) super.cardToCast).decremeantHealth(value);
+                else if (super.cardToCast instanceof Minion)
+                    ((Minion) super.cardToCast).decremeantHealth(value);
+                setCasted(true);
+            }
+        }else{
+            if (super.cardToCast instanceof Hero)
+                ((Hero) super.cardToCast).incrementHealth(value);
+            else if (super.cardToCast instanceof Minion)
+                ((Minion) super.cardToCast).incrementHealth(value);
+            destruct();
+        }
+        decrementCounters();
+    }
+}
