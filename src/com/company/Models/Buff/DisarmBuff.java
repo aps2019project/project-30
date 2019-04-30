60fcddafb5ba6acca16709c71c1084ef643d61ad
majidtaherkhani <majidtaherkhani555@gmail.com>
2019-04-30 16:15:47 +0430

    buff complited

--- src/com/company/Models/Buff/DisarmBuff.java
+++ src/com/company/Models/Buff/DisarmBuff.java
@@ -3,6 +3,8 @@ package com.company.Models.Buff;
 public class DisarmBuff extends Buff {
     public DisarmBuff(Type antiBuff, int remTurnToBeInactive, int remTurnToCast, int value) {
         super(antiBuff, remTurnToBeInactive, remTurnToCast, value);
+        super.name=Name.DISARM;
+        super.type=Type.NEGATIVE;
     }
 
     @Override
