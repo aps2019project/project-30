b0600257ece033d88fed3f73623fdf0470b3a50d
mhbahmani <mhbahmani79@gmail.com>
2019-05-01 22:05:18 +0430

    showMinion improved and removed to Minion

--- src/com/company/Models/Card/Minion/Minion.java
+++ src/com/company/Models/Card/Minion/Minion.java
@@ -6,13 +6,17 @@ import com.company.Models.Card.AttackType;
 import com.company.Models.Card.Card;
 import com.company.Models.Card.Hero.Hero;
 import com.company.Models.Card.Soldier;
+import com.company.Models.Card.Spell.Spell;
+import com.company.Models.Shop;
+
+import java.util.ArrayList;
 
 public class
 Minion extends Soldier {
     private ActivationTime activationTime;
 
     @Override
-    public Minion clone(){
+    public Minion clone() {
         Minion minion = new Minion();
         minion.setDescription(this.getDescription());
         minion.setInGraveCards(this.isInGraveCards());
@@ -25,13 +29,13 @@ Minion extends Soldier {
         return minion;
     }
 
-    public Minion makeCopyForCreatingNewCardInShop(){
+    public Minion makeCopyForCreatingNewCardInShop() {
         Minion minion = this.clone();
         minion.setId(Card.createNewCardIdToCreatingNewCardInShop());
         return minion;
     }
 
-    public Minion makeCopyForCreatingNewCardInBattle(){
+    public Minion makeCopyForCreatingNewCardInBattle() {
         Minion minion = this.clone();
         minion.setId(Card.createNewCardIdToCreatingNewCardInBattle(this.getName()));
         return minion;
@@ -43,5 +47,27 @@ Minion extends Soldier {
 
     public void setActivationTime(ActivationTime activationTime) {
         this.activationTime = activationTime;
+    }
+
+    public static void showCards(ArrayList<Card> cards) {
+        System.out.println("Cards :");
+        for (int i = 0; i < cards.size(); i++) {
+            if (cards.get(i) instanceof Minion) {
+                System.out.println((i + 1) + "- Type : Minion " +
+                        "- Name : " + cards.get(i).getName() +
+                        "- Class : " + ((Minion) cards.get(i)).getActivationTime() +
+                        "- AP : " + ((Minion) cards.get(i)).getAttackPower() +
+                        "- HP : " + ((Minion) cards.get(i)).getFullHealth() +
+                        "- MP : " + cards.get(i).getManaPoint() +
+                        "- Desc : " + Shop.getShopCollection().getCards().get(i).getDescription() +
+                        "- Sell cost : " + cards.get(i).getPriceInDrake());
+            } else if (cards.get(i) instanceof Spell) {
+                System.out.println((i + 1) + "- Type : Spell " +
+                        "- Name : " + cards.get(i).getName() +
+                        "- MP : " + cards.get(i).getManaPoint() +
+                        "- Desc : " + cards.get(i).getDescription() +
+                        "- Sell cost : " + cards.get(i).getPriceInDrake());
+            }
+        }
     }
 }
