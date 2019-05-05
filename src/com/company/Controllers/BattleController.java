29243218db2db0b3fd1342fa78e83b1e5798d17e
HaMiD <hamidrezash2000@gmail.com>
2019-05-05 17:23:35 +0430

    insert Card ConsoleInput Refactored

--- src/com/company/Controllers/BattleController.java
+++ src/com/company/Controllers/BattleController.java
@@ -474,6 +474,7 @@ public class BattleController {
                     cell.setCardInCell(newCard);
                     ((Soldier) newCard).setCell(cell);
                     Battle.getPlayingBattle().getTurnToPlay().getUsedCards().add(newCard);
+                    System.out.println(battle.getMap().toString());
                 } else {
                     ConsoleOutput.printErrorMessage(ErrorType.NOTENOUGH_MANA);
                 }
