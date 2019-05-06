0cc50d4e89c118406ed824fdb05086a957864792
majidtaherkhani <majidtaherkhani555@gmail.com>
2019-05-06 15:38:50 +0430

    sepahsalar fars added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -35,36 +35,36 @@ public class Main {
 //        StunBuff stunBuff = new StunBuff(null, 1, 0, 0);
 
 
+//
+//        Hero hero = new Hero();
+//        hero.setName("Simorgh");
+//
+//        hero.setPriceInDrake(9000);
+//        hero.setFullHealth(50);
+//        hero.setAttackPower(4);
+//        hero.setAttackType(AttackType.MELEE);
+////        hero.getBuffsToCast().add(stunBuff);
+//        hero.setManaPoint(5);
+//        hero.setCoolDown(8);
+//        hero.setTargetType(TargetType.FRIEND_HERO);
+//
+////        System.out.println("gson.toJson(hero) = " + gson.toJson(hero));
+//
+//        System.out.println("JsonController.getGson().toJson(hero) = " + JsonController.getGson().toJson(hero));
+//
+////        Gson gson = new Gson();
 
-        Hero hero = new Hero();
-        hero.setName("Simorgh");
 
-        hero.setPriceInDrake(9000);
-        hero.setFullHealth(50);
-        hero.setAttackPower(4);
-        hero.setAttackType(AttackType.MELEE);
-//        hero.getBuffsToCast().add(stunBuff);
-        hero.setManaPoint(5);
-        hero.setCoolDown(8);
-        hero.setTargetType(TargetType.FRIEND_HERO);
-
-//        System.out.println("gson.toJson(hero) = " + gson.toJson(hero));
-
-        System.out.println("JsonController.getGson().toJson(hero) = " + JsonController.getGson().toJson(hero));
-
-//        Gson gson = new Gson();
-
-
-        HealthPowerBuff healthPowerBuff = new HealthPowerBuff(null, 1, 0, 10);
+        //HealthPowerBuff healthPowerBuff = new HealthPowerBuff(null, 1, 0, 10);
         Minion minion = new Minion();
-        minion.setName("Oghab");
-        minion.setPriceInDrake(200);
-        minion.setManaPoint(2);
-        minion.setFullHealth(0);
-        minion.setAttackPower(2);
-        minion.setAttackType(AttackType.RANGED);
-        minion.getBuffsToCast().add(healthPowerBuff);
-        minion.setActivationTime(ActivationTime.ON_DEATH);
+        minion.setName("Sepahsalar Fars");
+        minion.setPriceInDrake(800);
+        minion.setManaPoint(7);
+        minion.setFullHealth(12);
+        minion.setAttackPower(4);
+        minion.setAttackType(AttackType.MELEE);
+        //minion.getBuffsToCast().add(healthPowerBuff);
+        minion.setActivationTime(ActivationTime.COMBO);
 //
 //
 //        ManaBuff manaBuff = new ManaBuff(null, 3, 0, 1);
@@ -75,15 +75,15 @@ public class Main {
 //        item.setDescription("ManaBuff : 3 Rounds - 1 Mana");
 //        item.setTargetType(TargetType.FRIEND_HERO);
 
-        StunBuff stunBuff= new StunBuff(null, 2, 0, 0);
-        Spell spell = new Spell();
-        spell.setName("Shock");
-        spell.setManaPoint(1);
-        spell.setPriceInDrake(1200);
-        spell.setTargetType(TargetType.ENEMY_SOLDIER);
-        spell.setInGraveCards(false);
-
-        spell.getBuffsToCast().add(stunBuff);
+//        StunBuff stunBuff= new StunBuff(null, 2, 0, 0);
+//        Spell spell = new Spell();
+//        spell.setName("Shock");
+//        spell.setManaPoint(1);
+//        spell.setPriceInDrake(1200);
+//        spell.setTargetType(TargetType.ENEMY_SOLDIER);
+//        spell.setInGraveCards(false);
+//
+//        spell.getBuffsToCast().add(stunBuff);
 
 
 
