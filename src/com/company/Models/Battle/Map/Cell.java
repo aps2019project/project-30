7f9aab8a1bd7a01ed00e7b89b29d4b725e5e1369
HaMiD <hamidrezash2000@gmail.com>
2019-05-06 18:49:26 +0430

    throwBuffsToSoldier method added

--- src/com/company/Models/Battle/Map/Cell.java
+++ src/com/company/Models/Battle/Map/Cell.java
@@ -1,9 +1,11 @@
 package com.company.Models.Battle.Map;
 
+import com.company.Models.Buff.AntiBuff;
 import com.company.Models.Buff.Buff;
 import com.company.Models.Card.Card;
 import com.company.Models.Card.Flag;
 import com.company.Models.Card.Item.Item;
+import com.company.Models.Card.Soldier;
 
 import java.util.ArrayList;
 
@@ -56,5 +58,19 @@ public class Cell {
 
     public void setCardInCell(Card cardInCell) {
         this.cardInCell = cardInCell;
+    }
+
+    public void throwBuffsToSoldier() {
+        int buffsCastedSizeBeforeThrow = cardInCell.getBuffsCasted().size();
+        for (Buff buff : cardInCell.getBuffsToCast()) {
+            Buff clonedBuff = buff.clone();
+            clonedBuff.setCardToCast(cardInCell);
+            cardInCell.getBuffsCasted().add(clonedBuff);
+        }
+        for (int i = 0; i < cardInCell.getBuffsCasted().size(); i++) {
+            Buff buff = cardInCell.getBuffsCasted().get(i);
+            if (buff instanceof AntiBuff || i > buffsCastedSizeBeforeThrow)
+                buff.cast();
+        }
     }
 }
