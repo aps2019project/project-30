b66b89c3d425d224bfb9921f5dd055089b27388c
HaMiD <hamidrezash2000@gmail.com>
2019-05-07 18:42:30 +0430

    Ghool Tak Cheshm Added To Minions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -57,42 +57,43 @@ public class Main {
 
 ////        StunBuff buff = new StunBuff(null,1, 0, 0);
 //        AntiBuff antiBuff = new AntiBuff(Buff.Name.DISARM, 1000, 0, 0);
-//        Minion minion = new Minion();
-//        minion.setName("Goraz Vahshi");
-//        minion.setPriceInDrake(500);
-//        minion.setManaPoint(6);
-//        minion.setFullHealth(10);
-//        minion.setAttackPower(14);
-//        minion.setAttackType(AttackType.MELEE);
-////        minion.getBuffsToCast().add(healthWeaknessBuff);
-//        minion.getBuffsCasted().add(antiBuff);
-//        minion.setActivationTime(ActivationTime.ON_DEFEND);
-//        minion.setTargetType(TargetType.ENEMY_SOLDIER);
+        HealthWeaknessBuff healthWeaknessBuff = new HealthWeaknessBuff(null, 1, 0, 2);
+        Minion minion = new Minion();
+        minion.setName("Ghool Tak Cheshm");
+        minion.setPriceInDrake(500);
+        minion.setManaPoint(7);
+        minion.setFullHealth(12);
+        minion.setAttackPower(11);
+        minion.setAttackType(AttackType.HYBRID);
+//        minion.getBuffsToCast().add(healthWeaknessBuff);
+        minion.getBuffsCasted().add(healthWeaknessBuff);
+        minion.setActivationTime(ActivationTime.ON_DEATH);
+        minion.setTargetType(TargetType.NEAR_ENEMYS_HERO);
+
 //
-//
-        AttackPowerBuff attackPowerBuff = new AttackPowerBuff(null, 1000, 0, 6);
-//        AttackPowerBuff attackPowerBuff = new AttackPowerBuff(null, 1000, 0, 1);
-        HealthPowerBuff healthPowerBuff = new HealthPowerBuff(null, 1000, 0, 2);
-        AttackWeaknessBuff attackWeaknessBuff = new AttackWeaknessBuff(null, 1000, 0, 2);
-        HolyBuff holyBuff = new HolyBuff(null, 2, 0, 10);
-        ManaBuff manaBuff = new ManaBuff(null, 1000, 0, 1);
-        HealthWeaknessBuff healthWeaknessBuff = new HealthWeaknessBuff(null, 1, 0, 8);
-//        ManaBuff manaBuff = new ManaBuff(null, 1, 1, 3);
-//        HolyBuff holyBuff = new HolyBuff(null,2,0,0);
-        Item item = new Item();
-        item.setName("King Wisdom");
-        item.setPriceInDrake(9000);
+//        AttackPowerBuff attackPowerBuff = new AttackPowerBuff(null, 1000, 0, 6);
+////        AttackPowerBuff attackPowerBuff = new AttackPowerBuff(null, 1000, 0, 1);
+//        HealthPowerBuff healthPowerBuff = new HealthPowerBuff(null, 1000, 0, 2);
+//        AttackWeaknessBuff attackWeaknessBuff = new AttackWeaknessBuff(null, 1000, 0, 2);
+//        HolyBuff holyBuff = new HolyBuff(null, 2, 0, 10);
+//        ManaBuff manaBuff = new ManaBuff(null, 1000, 0, 1);
+//        HealthWeaknessBuff healthWeaknessBuff = new HealthWeaknessBuff(null, 1, 0, 8);
+////        ManaBuff manaBuff = new ManaBuff(null, 1, 1, 3);
+////        HolyBuff holyBuff = new HolyBuff(null,2,0,0);
+//        Item item = new Item();
+//        item.setName("King Wisdom");
+//        item.setPriceInDrake(9000);
+////        item.getBuffsToCast().add(manaBuff);
 //        item.getBuffsToCast().add(manaBuff);
-        item.getBuffsToCast().add(manaBuff);
-//        item.getBuffsToCast().add(healthPowerBuff);
-        item.setDescription("increase mana by 1");
-        item.setTargetType(TargetType.WHOLE_FRIEND);
-        item.setName("Soul Eater");
-        item.setPriceInDrake(25000);
-        item.getBuffsToCast().add(attackPowerBuff);
-        //item.getBuffsToCast().add();
-        item.setDescription("with death of every friend minion, attack poewr buff will cast");
-        item.setTargetType(TargetType.RANDOM_FRIEND_SOLDIER);
-        System.out.println(JsonController.getGson().toJson(item));
+////        item.getBuffsToCast().add(healthPowerBuff);
+//        item.setDescription("increase mana by 1");
+//        item.setTargetType(TargetType.WHOLE_FRIEND);
+//        item.setName("Soul Eater");
+//        item.setPriceInDrake(25000);
+//        item.getBuffsToCast().add(attackPowerBuff);
+//        //item.getBuffsToCast().add();
+//        item.setDescription("with death of every friend minion, attack poewr buff will cast");
+//        item.setTargetType(TargetType.RANDOM_FRIEND_SOLDIER);
+        System.out.println(JsonController.getGson().toJson(minion));
     }
 }
