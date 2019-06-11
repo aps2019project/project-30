f29beaa7067060911a70ca058ceb2239b36635ad
majid taherkhani <majidtaherkhani555@gmail.com>
2019-06-11 02:57:57 -0700

    deck deleted added

--- src/com/company/Controllers/graphic/CollectionController.java
+++ src/com/company/Controllers/graphic/CollectionController.java
@@ -5,7 +5,13 @@ import com.company.Models.User.Account;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class CollectionController {
@@ -18,9 +24,29 @@ public class CollectionController {
     public void createDeck(ActionEvent actionEvent) {
         Account.getLoggedInAccount().getCollection().getCollectionController().createDeck(deckName.getText());
         if(creatDeckErrorType==null) {
-            Label deck = new Label(deckName.getText());
-            deck.getStyleClass().add("collection-deck");
-            deckContainer.getChildren().add(deck);
+            HBox hBox=new HBox();
+            hBox.getStyleClass().add("dechbox_collection");
+            Label decklabel = new Label(deckName.getText());
+            decklabel.getStyleClass().add("collection-deck");
+            hBox.getChildren().add(decklabel);
+            Image image = new Image("com/company/Views/graphic/images/waste-bin.png");
+            ImageView imageView = new ImageView(image);
+            imageView.setId(deckName.getText());
+            imageView.getStyleClass().add("deck_image_delete");
+            imageView.setFitHeight(20);
+            imageView.setFitWidth(20);
+            hBox.getChildren().add(imageView);
+            deckContainer.getChildren().add(hBox);
+
+            imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    Account.getLoggedInAccount().getCollection().getCollectionController().deleteDeck(((ImageView) event.getSource()).getId());
+                    deckContainer.getChildren().remove(hBox);
+                }
+            });
+
+
         }
     }
 
