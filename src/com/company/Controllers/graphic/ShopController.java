396305d6b2cfe5b69ce19477d04128119f4a0a8f
HaMiD <hamidrezash2000@gmail.com>
2019-06-13 11:21:06 +0430

    gifs updated

--- src/com/company/Controllers/graphic/ShopController.java
+++ src/com/company/Controllers/graphic/ShopController.java
@@ -1,31 +1,22 @@
 package com.company.Controllers.graphic;
 
 import com.company.Models.Card.Card;
+import com.company.Models.Card.Spell.Spell;
 import com.company.Models.Shop;
 import com.company.Models.User.Account;
 import com.company.Views.Graphic;
 import com.jfoenix.controls.JFXMasonryPane;
-import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-
-import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.StackPane;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-import java.util.Stack;
 
 public class ShopController implements Initializable {
     public JFXMasonryPane cardContainer;
@@ -51,13 +42,17 @@ public class ShopController implements Initializable {
 
                 try {
                     StackPane cardViewContainer = new StackPane();
-                    Image cardGif = new Image("com/company/Views/graphic/images/gifs/" + card.getName() + "_idle.gif");
+                    Image cardGif;
+                    if (card instanceof Spell)
+                        cardGif = new Image("com/company/Views/graphic/images/gifs/" + card.getName() + ".gif");
+                    else
+                        cardGif = new Image("com/company/Views/graphic/images/gifs/" + card.getName() + "_idle.gif");
                     ImageView cardView = new ImageView(cardGif);
                     cardViewContainer.getChildren().add(cardView);
                     cardContainer.getChildren().add(cardViewContainer);
                     cardViewContainer.setPrefWidth(200);
                     cardViewContainer.setPrefHeight(200);
-                } catch (NullPointerException|IllegalArgumentException e) {
+                } catch (NullPointerException | IllegalArgumentException e) {
                     System.out.println("===" + e.getMessage());
                 }
 
