d8eea4001ccbb5b8ca6f24063fe7d6e0a93b0425
mhbahmani <mhbahmani79@gmail.com>
2019-06-23 16:50:21 +0430

    battle scene music added

--- src/com/company/Controllers/graphic/ChooseGameController.java
+++ src/com/company/Controllers/graphic/ChooseGameController.java
@@ -1,6 +1,7 @@
 package com.company.Controllers.graphic;
 
 import com.company.Models.Battle.Battle;
+import com.company.Models.Sound;
 import com.company.Views.Graphic;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -36,6 +37,8 @@ public class ChooseGameController implements Initializable {
             Parent root = loader.load();
             ((GameController) loader.getController()).init();
             Graphic.stage.getScene().setRoot(root);
+            Sound.pause(Sound.MAIN_MENU_SOUND_ADDRESS);
+            Sound.play(Sound.BATTLE_MAIN_MUSIC_ADDRESS);
         } catch (IOException e) {
             e.printStackTrace();
         }
