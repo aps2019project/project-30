ed168e114b98d55b8026000f1b54f2162003ffc4
mhbahmani <mhbahmani79@gmail.com>
2019-06-23 16:35:09 +0430

    pause method added to Sound.java

--- src/com/company/Models/Sound.java
+++ src/com/company/Models/Sound.java
@@ -7,10 +7,12 @@ import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 import java.net.URISyntaxException;
+import java.util.HashMap;
 
 public class Sound {
 
     final public static String MAIN_MENU_SOUND_ADDRESS = "graphic/sounds/mainmenu.mp3";
+    private static HashMap<String, MediaPlayer> playedSongs = new HashMap<>();
 
     public static void play(String soundAddress) {
         Media media = null;
@@ -20,6 +22,12 @@ public class Sound {
             e.printStackTrace();
         }
         MediaPlayer mediaPlayer = new MediaPlayer(media);
+        playedSongs.put(soundAddress, mediaPlayer);
         mediaPlayer.play();
+    }
+
+    public static void pause(String soundAddress) {
+        playedSongs.get(soundAddress).pause();
+        playedSongs.remove(soundAddress);
     }
 }
