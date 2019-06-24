531d4ed780c645329f6b800b27a143e468ce8909
HaMiD <hamidrezash2000@gmail.com>
2019-06-24 14:36:17 +0430

    combo box added to create card

--- src/com/company/Controllers/graphic/CustomCardController.java
+++ src/com/company/Controllers/graphic/CustomCardController.java
@@ -1,8 +1,25 @@
 package com.company.Controllers.graphic;
 
+import com.company.Models.Card.AttackType;
+import com.jfoenix.controls.JFXComboBox;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
-public class CustomCardController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomCardController implements Initializable {
+    public JFXComboBox<AttackType> heroAttackType;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        heroAttackType.getItems().addAll(AttackType.values());
+//        heroAttackType.getSelectionModel().getSelectedItem()
+//        todo: to get selected item
+    }
+
     public void createHero(ActionEvent actionEvent) {
     }
 
