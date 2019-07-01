2684d91029ea76dcd9ab73b4d7505fe2e7cd5eea
mhbahmani <mhbahmani79@gmail.com>
2019-07-01 19:15:26 +0430

    writing port number of file added to Server.java

--- src/com/company/Models/Server.java
+++ src/com/company/Models/Server.java
@@ -1,4 +1,30 @@
 package com.company.Models;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Properties;
+
 public class Server {
+    final private static String PORT_NUMBER_FILE_ADDRESS = "config.properties";
+
+    public static void main(String[] args) throws IOException{
+        ServerSocket server = new ServerSocket(0);
+        setPortNumberOnPropertiesFile(server);
+    }
+
+    private static void setPortNumberOnPropertiesFile(ServerSocket server) throws IOException {
+        File portNumberFile = new File(PORT_NUMBER_FILE_ADDRESS);
+        if(!portNumberFile.exists()){
+            portNumberFile.createNewFile();
+        }
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(portNumberFile));
+        String port = String.valueOf(server.getLocalPort());
+        System.out.println(port);
+        properties.setProperty("port",port);
+        properties.store(new FileOutputStream(PORT_NUMBER_FILE_ADDRESS),null);
+    }
 }
