a18091aa9eb70522a99ad7c7e33d1147075f5015
mhbahmani <mhbahmani79@gmail.com>
2019-07-02 18:54:29 +0430

    run and writeln methods implemented in MessageWriter

--- src/com/company/Controllers/MessageWriter.java
+++ src/com/company/Controllers/MessageWriter.java
@@ -7,6 +7,29 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class MessageWriter extends Thread {
+    private PrintStream printer;
+    private BlockingQueue<String> pipeline = new LinkedBlockingQueue<>();
 
+    MessageWriter(OutputStream output) {
+        printer = new PrintStream(output);
+    }
 
+    @Override
+    public void run() {
+        try {
+            while (!interrupted()) {
+                printer.println(pipeline.take());
+            }
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void writeln(String message) {
+        try {
+            pipeline.put(message);
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
