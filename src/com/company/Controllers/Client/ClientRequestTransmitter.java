3fee08c6670b09834884cf4038f80dd3d1564f5b
mhbahmani <mhbahmani79@gmail.com>
2019-07-03 00:41:59 +0430

    ClientRequestTransmitter class implemented

--- src/com/company/Controllers/Client/ClientRequestTransmitter.java
+++ src/com/company/Controllers/Client/ClientRequestTransmitter.java
@@ -1,4 +1,33 @@
 package com.company.Controllers.Client;
 
-public class ClientRequestTransmitter {
+import com.company.Models.Request;
+import com.google.gson.Gson;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ClientRequestTransmitter extends Thread{
+    private PrintStream printer;
+    private BlockingQueue<Request> requests = new LinkedBlockingQueue<>();
+
+    public ClientRequestTransmitter(OutputStream output) {
+        printer = new PrintStream(output);
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!interrupted()) {
+                printer.println(new Gson().toJson(requests.take()));
+            }
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void sendRequest (Request request) {
+        requests.add(request);
+    }
 }
